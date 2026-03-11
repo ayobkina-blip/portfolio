351f75438ae1dd5fd2cc8f59e4590da61214a37d
ayobkina-blip <ayobkina@gmail.com>
2026-03-11 07:20:28 +0100

    fix bug visual hero

--- frontend/src/app/components/navbar/navbar.ts
+++ frontend/src/app/components/navbar/navbar.ts
@@ -1,25 +1,50 @@
 import { Component, HostListener } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
+
 import { ThemeToggleComponent } from '../theme-toggle/theme-toggle.component';
 
+
+
 @Component({
+
   selector: 'app-navbar',
+
   imports: [CommonModule, ThemeToggleComponent],
+
   templateUrl: './navbar.html',
+
   styleUrl: './navbar.css',
+
 })
+
 export class NavbarComponent {
+
   hasScrolled = false;
 
+
+
   @HostListener('window:scroll', [])
+
   onWindowScroll() {
+
     this.hasScrolled = window.scrollY > 10;
+
   }
 
+
+
   scrollToSection(sectionId: string): void {
+
     const element = document.getElementById(sectionId);
+
     if (element) {
+
       element.scrollIntoView({ behavior: 'smooth' });
+
     }
+
   }
+
 }
+
